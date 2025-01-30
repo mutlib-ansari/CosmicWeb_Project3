c18548010c6fa4287a41e9dc7c4efb8e58833b5d
dpknayak11 <dpknayak111@gmail.com>
2025-01-30 11:07:20 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,25 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()] ,
+//   resolve: {
+//     alias: {
+//       'mdb-react-ui-kit': 'path-to-installed-module/mdb-react-ui-kit',
+//     },
+//   },
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()] ,
-  resolve: {
-    alias: {
-      'mdb-react-ui-kit': 'path-to-installed-module/mdb-react-ui-kit',
-    },
+  plugins: [react()],
+  build: {
+    outDir: "dist",
   },
-})
+});
